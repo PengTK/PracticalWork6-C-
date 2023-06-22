d07eb25d298d79f19e1885e9d78344233224a595
PengTK <114110877+PengTK@users.noreply.github.com>
2023-06-22 05:19:14 +0300

    Добавьте файлы проекта.

---
+++ PracticalWork6 C++/PracticalWork6 C++.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "..\DynamicLib\pch.h"
+using namespace std;
+
+
+typedef bool (*FindCharsFunc)(const char*, const char*);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string str;
+    string chars;
+    cout << "Введите текст: ";
+    cin >> str;
+    cout << "Введите символы для поиска: ";
+    cin >> chars;
+
+    HINSTANCE lib = LoadLibrary(TEXT("DynamicLib.dll"));
+
+    FindCharsFunc findChars = (FindCharsFunc)GetProcAddress(lib, "findChars");
+ 
+
+    bool result = findChars(str.c_str(), chars.c_str());
+    cout << "Результат: " << (result ? "true" : "false") << endl;
+
+    FreeLibrary(lib);
+
+    return 0;
+}
